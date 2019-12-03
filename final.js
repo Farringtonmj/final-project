07ef959fdbaad295878189fabded7c1b751f5545
Megan Farrington <farringtonmj@s.dcsdk12.org>
2019-12-03 09:25:43 -0700

    commit with add working infinite times

--- final.js
+++ final.js
@@ -4,39 +4,35 @@ function ready () {
     let mltply = document.strt.mlt;
     let dvd = document.strt.dvd;
     let strt = document.strt;
+    let addition = document.addition;
+    let i = 0
 
-
-    sbtrct.onsubmit = function (e) {
-        e.preventDefault();
-    }
-    mltply.onsubmit = function (e) {
-        e.preventDefault();
-    }
-    dvd.onsubmit = function (e) {
-        e.preventDefault();
-    }
 
     strt.onsubmit = function (e) {
         e.preventDefault();
     }
-    add.onclick = function () {
+    
+    add.onclick = function add() {
         strt.classList.add("hidden")
         let addQuests = []
         addQuests.push(document.addition.Q1)
         let inptbx = addQuests[0];
+        console.log(inptbx)
         let question = document.createElement('question');
         let number1 = Math.floor(Math.random() * 60);
         let number2 = Math.floor(Math.random() * 60);
         question.innerHTML = '<p>' + number1 + ' + ' + number2 + '</p>';
         document.body.append(question);
         inptbx.classList.remove("hidden")
-        inptbx.onsubmit = function (e) {
+        addition.onsubmit = function (e) {
             e.preventDefault();
         }
         inptbx.onchange = function () {
             let answer = this.value
             if (answer == number1 + number2) {
-                console.log("ok")
+                this.value = ""
+                question.classList.add('hidden')
+                add();
             }
         }
     }
