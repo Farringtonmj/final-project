a27bdb2d5043974ad9a19a7c3a058dd1b3102d34
Megan Farrington <farringtonmj@s.dcsdk12.org>
2019-12-06 09:11:32 -0700

    Version with timer working for all 4

--- final.js
+++ final.js
@@ -6,7 +6,11 @@ function ready () {
     let dvd = document.strt.dvd;
     let strt = document.strt;
     let addition = document.addition;
+    let start = document.start.start;
+    let startForm = document.start;
     let i = 0
+    let m = 0;
+    let n = 0
     let timeSpent = 0
     let time = document.createElement('time')
     time.className = "timer"
@@ -15,155 +19,215 @@ function ready () {
     strt.onsubmit = function (e) {
         e.preventDefault();
     }
-
     function timer () {
         document.body.append(time)
         function count() {
             timeSpent++;
             document.body.prepend(time);
-            time.innerHTML = "<h2>" + timeSpent + "<h2>"
+            if (timeSpent < 60) {
+                m = m
+            } else {
+                m++
+                timeSpent = 0
+            }
+            if (timeSpent < 10) {
+                time.innerHTML = "<h2>"+ m + ':' + n  + timeSpent + "<h2>"
+            } else {
+                time.innerHTML = "<h2>"+ m + ':'  + timeSpent + "<h2>"
+            }
+            
             document.body.append(time)
             console.log(timeSpent)
         }
-        setTimeout(() => count(), 100);
+        if (i < 30) {
+            //setInterval and setTimeout are things I learned in trying to make this timer
+            setInterval(() => count(), 1000);
+        } else {
+            alert("h")
+            let endTime = document.createElement('time')
+            endTime.innerHTML = '<h1>Your time was: ' + timeSpent + ' seconds</h1>'
+            document.body.append(endTime)
+            // window.location.reload();
+        }
     }
+
     
-    add.onclick = function add() {
-        let ding = document.getElementById("ding");
+    add.onclick = function() {
+        timer();
         strt.classList.add("hidden")
-        let addQuests = []
-        addQuests.push(document.addition.Q1)
-        let inptbx = addQuests[0];
-        let question = document.createElement('question');
-        let number1 = Math.floor(Math.random() * 60);
-        let number2 = Math.floor(Math.random() * 60);
-        question.innerHTML = '<p>' + number1 + ' + ' + number2 + '</p>';
-        document.body.append(question);
-        inptbx.classList.remove("hidden")
-        addition.onsubmit = function (e) {
+        console.log(start)
+        start.classList.remove("hidden")
+        startForm.onsubmit = function(e) {
             e.preventDefault();
         }
-        inptbx.onchange = function () {
-            let answer = this.value
-            if (answer == number1 + number2) {
-                this.value = ""
-                ding.play();
-                question.classList.add('hidden')
-                if (i < 30) {
-                    add();
-                } else {
-                    i = 0
-                    window.location.reload();
+        start.onclick = function add() {
+            start.classList.add("hidden")
+            let ding = document.getElementById("ding");
+            strt.classList.add("hidden")
+            let addQuests = []
+            addQuests.push(document.addition.Q1)
+            let inptbx = addQuests[0];
+            let question = document.createElement('question');
+            let number1 = Math.floor(Math.random() * 60);
+            let number2 = Math.floor(Math.random() * 60);
+            question.innerHTML = '<p>' + number1 + ' + ' + number2 + '</p>';
+            document.body.append(question);
+            inptbx.classList.remove("hidden")
+            addition.onsubmit = function (e) {
+                e.preventDefault();
+            }
+            inptbx.onchange = function () {
+                let answer = this.value
+                if (answer == number1 + number2) {
+                    this.value = ""
+                    ding.play();
+                    question.classList.add('hidden')
+                    if (i < 30) {
+                        add();
+                    } else {
+                        i = 0
+                        window.location.reload();
+                    }
+                    i++
                 }
-                i++
             }
         }
-    }
+    }   
 
-    sbtrct.onclick = function sbtrct() {
-        let ding = document.getElementById("ding");
+    sbtrct.onclick = function() {
+        timer();
         strt.classList.add("hidden")
-        let sbtrctQuests = []
-        sbtrctQuests.push(document.subtraction.Q2)
-        let inptbx = sbtrctQuests[0];
-        console.log(inptbx)
-        inptbx.classList.remove("hidden")
-        let question = document.createElement('question');
-        let number1 = Math.floor(Math.random() * 60);
-        let number2 = Math.floor(Math.random() * 60);
-        question.innerHTML = '<p>' + number1 + ' - ' + number2 + '</p>';
-        document.body.append(question);
-        subtraction.onsubmit = function (e) {
+        console.log(start)
+        start.classList.remove("hidden")
+        startForm.onsubmit = function(e) {
             e.preventDefault();
         }
-        inptbx.onchange = function () {
-            let answer = this.value
-            if (answer == number1 - number2) {
-                this.value = ""
-                ding.play();
-                question.classList.add('hidden')
-                if (i < 30) {
-                    sbtrct();
-                } else {
-                    i = 0
-                    window.location.reload();
-                }
-                i++
+        start.onclick = function sbtrct() {
+            start.classList.add("hidden")
+            let ding = document.getElementById("ding");
+            strt.classList.add("hidden")
+            let sbtrctQuests = []
+            sbtrctQuests.push(document.subtraction.Q2)
+            let inptbx = sbtrctQuests[0];
+            inptbx.classList.remove("hidden")
+            let question = document.createElement('question');
+            let number1 = Math.floor(Math.random() * 60);
+            let number2 = Math.floor(Math.random() * 60);
+            while (number1 < number2) {
+                number1 = Math.floor(Math.random() * 60);
+                number2 = Math.floor(Math.random() * 60);
             }
-        }
-
-    }
-    mltply.onclick = function mltply() {
-        let ding = document.getElementById("ding");
-        strt.classList.add("hidden")
-        let mltplyQuests = []
-        mltplyQuests.push(document.multiplication.Q3)
-        let inptbx = mltplyQuests[0];
-        console.log(inptbx)
-        inptbx.classList.remove("hidden")
-        let question = document.createElement('question');
-        let number1 = Math.floor(Math.random() * 12);
-        let number2 = Math.floor(Math.random() * 12);
-        question.innerHTML = '<p>' + number1 + ' x ' + number2 + '</p>';
-        document.body.append(question);
-        multiplication.onsubmit = function (e) {
-            e.preventDefault();
-        }
-        inptbx.onchange = function () {
-            let answer = this.value
-            if (answer == number1 * number2) {
-                this.value = ""
-                ding.play();
-                question.classList.add('hidden')
-                if (i < 30) {
-                    mltply();
-                } else {
-                    i = 0
-                    window.location.reload();
+            question.innerHTML = '<p>' + number1 + ' - ' + number2 + '</p>';
+            document.body.append(question);
+            subtraction.onsubmit = function (e) {
+                e.preventDefault();
+            }
+            inptbx.onchange = function () {
+                let answer = this.value
+                if (answer == number1 - number2) {
+                    this.value = ""
+                    ding.play();
+                    question.classList.add('hidden')
+                    if (i < 30) {
+                        sbtrct();
+                    } else {
+                        i = 0
+                        window.location.reload();
+                    }
+                    i++
                 }
-                i++
             }
         }
 
     }
-    dvd.onclick = function dvd() {
-
-        let ding = document.getElementById("ding");
+    mltply.onclick = function() {
+        timer();
         strt.classList.add("hidden")
-        let dvdQuests = []
-        dvdQuests.push(document.division.Q1)
-        let inptbx = dvdQuests[0];
-        console.log(inptbx)
-        inptbx.classList.remove("hidden")
-        let question = document.createElement('question');
-        let number1 = Math.floor(Math.random() * 40);
-        let number2 = Math.floor(Math.random() * 40);
-        while (number1 % number2 != 0) {
-            number1 = Math.floor(Math.random() * 40);
-            number2 = Math.floor(Math.random() * 40);
-        }
-        question.innerHTML = '<p> ' + number1 + '/' + number2 + ' </p>';
-        document.body.append(question);
-        division.onsubmit = function (e) {
+        console.log(start)
+        start.classList.remove("hidden")
+        startForm.onsubmit = function(e) {
             e.preventDefault();
         }
-        inptbx.onchange = function () {
-            let answer = this.value
-            if (answer == number1 / number2) {
-                i ++
-                this.value = ""
-                ding.play();
-                question.classList.add('hidden')
-                if (i < 30) {
-                    dvd();
-                } else {
-                    i = 0
-                    window.location.reload();
+        start.onclick = function mltply() {
+            start.classList.add("hidden")
+            document.body.append(time)
+            let ding = document.getElementById("ding");
+            strt.classList.add("hidden")
+            let mltplyQuests = []
+            mltplyQuests.push(document.multiplication.Q3)
+            let inptbx = mltplyQuests[0];
+            inptbx.classList.remove("hidden")
+            let question = document.createElement('question');
+            let number1 = Math.floor(Math.random() * 12);
+            let number2 = Math.floor(Math.random() * 12);
+            question.innerHTML = '<p>' + number1 + ' x ' + number2 + '</p>';
+            document.body.append(question);
+            multiplication.onsubmit = function (e) {
+                e.preventDefault();
+            }
+            inptbx.onchange = function () {
+                let answer = this.value
+                if (answer == number1 * number2) {
+                    this.value = ""
+                    ding.play();
+                    question.classList.add('hidden')
+                    if (i < 30) {
+                        mltply();
+                    } else {
+                        i = 0
+                        window.location.reload();
+                    }
+                    i++
                 }
-                
             }
         }
+
     }
+    dvd.onclick = function() {
+        timer();
+        strt.classList.add("hidden")
+        console.log(start)
+        start.classList.remove("hidden")
+        startForm.onsubmit = function(e) {
+            e.preventDefault();
+        }
+        start.onclick = function dvd() {
+            start.classList.add("hidden")
+            let ding = document.getElementById("ding");
+            let dvdQuests = []
+            dvdQuests.push(document.division.Q1)
+            let inptbx = dvdQuests[0];
+            inptbx.classList.remove("hidden")
+            let question = document.createElement('question');
+            let number1 = Math.floor(Math.random() * 40);
+            let number2 = Math.floor(Math.random() * 40);
+            while (number1 % number2 != 0) {
+                number1 = Math.floor(Math.random() * 40);
+                number2 = Math.floor(Math.random() * 40);
+            }
+            question.innerHTML = '<p> ' + number1 + '/' + number2 + ' </p>';
+            document.body.append(question);
+            division.onsubmit = function (e) {
+                e.preventDefault();
+            }
+            inptbx.onchange = function () {
+                let answer = this.value
+                if (answer == number1 / number2) {
+                    i ++
+                    this.value = ""
+                    ding.play();
+                    question.classList.add('hidden')
+                    if (i < 2) {
+                        dvd();
+                    } else {
+                        i = 0
+                        window.location.reload();
+                    }
+
+                }
+            }
+        }
+    }   
 
 }
 
